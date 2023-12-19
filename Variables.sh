546543f10c7ef8bab8c162d5a689adadda4a0b14
shaikariii <shaikarif0207@gmail.com>
2023-12-19 11:15:35 +0530

    add changes

--- Variables.sh
+++ Variables.sh
@@ -8,4 +8,4 @@ echo "$person1 is the first name"
 echo "$person2 is the last name"
 
 Date=$(date)
-echo "The script is executed at timestamp : $DATE"
+echo "The script is executed at timestamp : $Date"
