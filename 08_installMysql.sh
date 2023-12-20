ba7d4f76607763199b737cdd6b3251576a84121b
shaikariii <shaikarif0207@gmail.com>
2023-12-20 11:57:45 +0530

    added conditions

--- 08_installMysql.sh
+++ 08_installMysql.sh
@@ -16,5 +16,15 @@ then
     echo "Installation of mysql is failed"
     exit 1
 else    
-    echo "Installation is success"
+    echo "Installation of mysql is success"
+fi
+
+yum install postfix -y
+
+if [ #? -ne 0 ]
+then
+    echo "Installation of Postfix is failed"
+    exit 1
+else
+    echo "Installation of postfix is success"
 fi
